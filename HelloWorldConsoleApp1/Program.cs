aa40193d02739b40cbcd13e56d745ef5edbe7487
Michaylov Victor <fox.promo@mail.ru>
2022-05-05 20:32:29 +0700

    Добавьте файлы проекта.

---
+++ HelloWorldConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+    internal class Program
+    {
+        static void Main()
+        {
+        WriteLine("Hello World!!!");     // Вывод текста в консоли
+
+        ReadKey();                      // Ожидание от пользователя
+                                        // нажатия какой-либо клавиши
+        }
+    }
